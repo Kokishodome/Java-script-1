002780e6cc9f49005bab6571cae37c6825e29c1a
Kokishodome <159949398+Kokishodome@users.noreply.github.com>
2024-07-16 20:02:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Judo");
+let nickname="Unicornprincess345"
+console.log("username: "+nickname);
+console.log(nickname+" has 20 friends");
+let judoTitle = document.getElementById("Judo")
+console.log(judoTitle);
+judoTitle.onclick = function() {
+judoTitle.innerHTML="Judo is great"
+}
